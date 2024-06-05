24c8fef2d9f60d55b3727dc606c3804e55e8624e
AjayGohel13 <167311130+AjayGohel13@users.noreply.github.com>
2024-06-05 22:25:19 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
 			},
 			{
 				protocol: "https",
-				hostname: "https://mydigitalmarket.toystack.dev",
+				hostname: "digital-market-place-three.vercel.app/",
 			},
 		],
 	},
